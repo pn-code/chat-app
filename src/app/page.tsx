52c7390e1476a4c54f2236cd82d374b00998ea0a
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-08 22:16:17 -0700

    Redis & Next-Auth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Button from '@/components/ui/Button'
+import { db } from '@/lib/db'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default async function Home() {
+
   return (
    <Button>Hello World</Button>
   )
