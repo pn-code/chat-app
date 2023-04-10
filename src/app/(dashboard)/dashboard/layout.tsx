8f3e728f7ec5e509d68f1cc7685d43dceacb5a6f
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-10 16:44:48 -0700

    Added user info section to dashboard layout

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -64,10 +64,37 @@ export default async function Layout({ children }: LayoutProps) {
 							))}
 						</ul>
 					</li>
-				</ul>
-			</nav>
 
+					<li className='-mx-6 mt-auto flex items-center'>
+              <div className='flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900'>
+                <div className='relative h-8 w-8 bg-gray-50'>
+                  <Image
+                    fill
+                    referrerPolicy='no-referrer'
+                    className='rounded-full'
+                    src={session.user.image || ''}
+                    alt='Your profile picture'
+                  />
+                </div>
+
+                <span className='sr-only'>Your profile</span>
+                <div className='flex flex-col'>
+                  <span aria-hidden='true'>{session.user.name}</span>
+                  <span className='text-xs text-zinc-400' aria-hidden='true'>
+                    {session.user.email}
+                  </span>
+                </div>
+              </div>
+
+
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <aside className='max-h-screen container py-16 md:py-12 w-full'>
+        {children}
+      </aside>
 			</div>
-			{children}</div>
 	);
 }
