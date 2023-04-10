b68bca2f9aa58fae64173cb2c8a7d274e279ac38
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-09 20:43:51 -0700

    Built out AddFriend Form

---
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -0,0 +1,13 @@
+import AddFriendForm from '@/components/AddFriendForm'
+import React from 'react'
+
+function AddFriendsPage() {
+  return (
+    <main className='pt-8'>
+        <h1 className='font-bold text-5xl mb-8'>Add a friend</h1>
+        <AddFriendForm/>
+    </main>
+  )
+}
+
+export default AddFriendsPage
---
+++ src/components/AddFriendForm.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React from "react";
+import Button from "./ui/Button";
+
+function AddFriendForm() {
+  return (
+    <form className="max-w-sm">
+      <label
+        htmlFor="email"
+        className="block text-sm font-medium leading-6 text-gray-900"
+      >
+        Add friend by email
+      </label>
+
+      <div className="mt-2 flex gap-4">
+        <input
+          className="block w-full rounded-md border-0 py-1.5 text-slate-900 shadow-sm ring-1 ring-inset ring-slate-300 placeholder:text-slate-400 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          type="email"
+          placeholder="you@example.com"
+        />
+      </div>
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+export default AddFriendForm;
