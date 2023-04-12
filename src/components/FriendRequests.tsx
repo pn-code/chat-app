409971300dfc5376f3e24ea31d868c83760db03d
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-12 15:42:59 -0700

    Implementing reject/add friend on front-end

--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -1,6 +1,8 @@
 "use client";
+import axios from "axios";
 import { X } from "lucide-react";
 import { Check, UserPlus } from "lucide-react";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 interface FriendRequestsPage {
@@ -15,7 +17,29 @@ function FriendRequests({
 	const [friendRequests, setFriendRequests] = useState<IncomingFriendRequest[]>(
 		incomingFriendRequests
 	);
-    
+
+	const router = useRouter();
+
+	const acceptFriend = async (senderId: string) => {
+		await axios.post("/api/friends/accept", { id: senderId });
+
+		setFriendRequests((prev) =>
+			prev.filter((request) => senderId !== request.senderId)
+		);
+
+		router.refresh();
+	};
+
+	const rejectFriend = async (senderId: string) => {
+		await axios.post("/api/friends/reject", { id: senderId });
+
+		setFriendRequests((prev) =>
+			prev.filter((request) => senderId !== request.senderId)
+		);
+
+		router.refresh();
+	};
+
 	return (
 		<>
 			{friendRequests.length === 0 ? (
@@ -26,12 +50,14 @@ function FriendRequests({
 						<UserPlus className="text-black" />
 						<p className="font-medium text-lg">{request.senderEmail}</p>
 						<button
+							onClick={() => acceptFriend(request.senderId)}
 							aria-label="accept friend"
 							className="w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md"
 						>
 							<Check className="font-semibold text-white w-3/4 h-3/4" />
 						</button>
-                        <button
+						<button
+							onClick={() => rejectFriend(request.senderId)}
 							aria-label="reject friend"
 							className="w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md"
 						>
