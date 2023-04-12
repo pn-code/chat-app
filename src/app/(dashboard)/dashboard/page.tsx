dd607daf0f2f5908704f4a74cb1376a57f11404d
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-12 15:56:23 -0700

    Removing user info & replaced with temporary text

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,10 +1,4 @@
-import { authOptions } from "@/lib/auth"
-import { getServerSession } from "next-auth"
-
-
 export default async function DashboardPage () {
-    const session = await getServerSession(authOptions)
-
-    return <pre>{JSON.stringify(session)}</pre>
+    return <pre>Dashboard</pre>
 
 }
