2cf89d4d21f90c78c39fda89a4c9d9cbc7891b43
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-12 14:06:34 -0700

    Built out FriendRequestsPage

---
+++ src/app/(dashboard)/dashboard/requests/page.tsx
@@ -0,0 +1,39 @@
+import { fetchRedis } from "@/app/helpers/redis";
+import FriendRequests from "@/components/FriendRequests";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
+import React from "react";
+
+async function FriendRequestsPage() {
+	const session = await getServerSession(authOptions);
+	if (!session) notFound();
+
+	// Ids of people who sent the current user a friend request...
+	const incomingSenderIds = (await fetchRedis(
+		"smembers",
+		`user:${session.user.id}:incoming_friend_request`
+	)) as string[];
+
+	const incomingFriendRequests = await Promise.all(
+		incomingSenderIds.map(async (senderId) => {
+			const senderResult = (await fetchRedis("get", `user:${senderId}`)) as string;
+			const sender = JSON.parse(senderResult)
+			
+			return {
+				senderId,
+				senderEmail: sender.email,
+			};
+		})
+	);
+	return (
+		<main className="pt-8">
+			<h1 className="font-bold text-5xl mb-8">Add a friend</h1>
+            <div className="flex flex-col gap-4">
+                <FriendRequests incomingFriendRequests={incomingFriendRequests} sessionId={session.user.id}/>
+            </div>
+		</main>
+	);
+}
+
+export default FriendRequestsPage;
