464ad438023a61114c1277e00becd55677edbe45
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-12 17:47:13 -0700

    Built out getFriends helper

---
+++ src/app/helpers/getFriendsByUserId.ts
@@ -0,0 +1,20 @@
+import { fetchRedis } from "./redis";
+
+export const getFriendsByUserId = async (userId: string) => {
+	// Retrieve friends for current user...
+	const friendIds = (await fetchRedis(
+		"smembers",
+		`user:${userId}:friends`
+	)) as string[];
+
+	// Get user data
+	const friends = await Promise.all(
+		friendIds.map(async (friendId) => {
+			const friend = (await fetchRedis("get", `user:${friendId}`)) as User;
+			return friend;
+		})
+	);
+
+    // Return array of friends
+	return friends;
+};
