a66f6b8cb4b5e0fd732ad049276f077f306ac919
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-13 17:20:40 -0700

    Notify user of message if not on same page

--- src/components/ChatListSidebar.tsx
+++ src/components/ChatListSidebar.tsx
@@ -3,10 +3,17 @@ import { pusherClient } from "@/lib/pusher";
 import { chatHrefConstructor, toPusherKey } from "@/lib/utils";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import UnseenChatToast from "./UnseenChatToast";
 
 interface ChatListSidebarProps {
 	friends: User[];
 	sessionId: string;
+}
+
+interface ExtendedMessage extends Message {
+	senderImg: string;
+	senderName: string;
 }
 
 function ChatListSidebar({ friends, sessionId }: ChatListSidebarProps) {
@@ -16,28 +23,46 @@ function ChatListSidebar({ friends, sessionId }: ChatListSidebarProps) {
 	const [unseenMessages, setUnseenMessages] = useState<Message[]>([]);
 
 	useEffect(() => {
-		pusherClient.subscribe(toPusherKey(`user:${sessionId}:chats`))
-		pusherClient.subscribe(toPusherKey(`user:${sessionId}:friends`))
-	
+		pusherClient.subscribe(toPusherKey(`user:${sessionId}:chats`));
+		pusherClient.subscribe(toPusherKey(`user:${sessionId}:friends`));
+
 		const newFriendHandler = () => {
-		  console.log("received new user")
-		}
-	
-		const chatHandler = () => {
-		  console.log("new chat msg")
-		}
-	
-		pusherClient.bind('new_message', chatHandler)
-		pusherClient.bind('new_friend', newFriendHandler)
-	
+			console.log("received new user");
+		};
+
+		const chatHandler = (message: ExtendedMessage) => {
+			const shouldNotify =
+				pathname !==
+				`/dashboard/chat/${chatHrefConstructor(sessionId, message.senderId)}`;
+
+			if (!shouldNotify) return;
+
+			// notify the user
+			toast.custom((t) => (
+				<UnseenChatToast
+					t={t}
+					sessionId={sessionId}
+					senderId={message.senderId}
+					senderImg={message.senderImg}
+					senderMessage={message.text}
+					senderName={message.senderName}
+				/>
+			));
+
+			setUnseenMessages((prev) => [...prev, message]);
+		};
+
+		pusherClient.bind("new_message", chatHandler);
+		pusherClient.bind("new_friend", newFriendHandler);
+
 		return () => {
-		  pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:chats`))
-		  pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:friends`))
-	
-		  pusherClient.unbind('new_message', chatHandler)
-		  pusherClient.unbind('new_friend', newFriendHandler)
-		}
-	  }, [pathname, sessionId, router])
+			pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:chats`));
+			pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:friends`));
+
+			pusherClient.unbind("new_message", chatHandler);
+			pusherClient.unbind("new_friend", newFriendHandler);
+		};
+	}, [pathname, sessionId, router]);
 
 	// If user checks chat, update unseen messages count...
 	useEffect(() => {
@@ -49,7 +74,7 @@ function ChatListSidebar({ friends, sessionId }: ChatListSidebarProps) {
 	}, [pathname]);
 
 	return (
-		<ul role="list" className="max-h-[25rem] overflow-y-auto -mx-2 space-y-1">
+		<ul role="list" className="-mx-2 max-h-[25rem] space-y-1 overflow-y-auto">
 			{friends.sort().map((friend) => {
 				const unseenMessagesCount = unseenMessages.filter((unseenMsg) => {
 					return unseenMsg.senderId === friend.id;
@@ -58,7 +83,7 @@ function ChatListSidebar({ friends, sessionId }: ChatListSidebarProps) {
 				return (
 					<li key={friend.id}>
 						<a
-							className="text-slate-700 hover:text-indigo-600 hover:bg-slate-50 group flex items-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+							className="group flex items-center gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-slate-700 hover:bg-slate-50 hover:text-indigo-600"
 							href={`/dashboard/chat/${chatHrefConstructor(
 								sessionId,
 								friend.id
@@ -66,7 +91,7 @@ function ChatListSidebar({ friends, sessionId }: ChatListSidebarProps) {
 						>
 							{friend.name}
 							{unseenMessagesCount > 0 && (
-								<div className="bg-indigo-600 font-medium text-xs text-white w-4 h-4 rounded-full flex justify-center items-center">
+								<div className="flex h-4 w-4 items-center justify-center rounded-full bg-indigo-600 text-xs font-medium text-white">
 									{unseenMessagesCount}
 								</div>
 							)}
