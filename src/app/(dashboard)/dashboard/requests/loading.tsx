1d0be585bab5973ca55e0502e022d3a97a4f64ed
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-13 18:15:24 -0700

    Added loading states for pages

---
+++ src/app/(dashboard)/dashboard/requests/loading.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import 'react-loading-skeleton/dist/skeleton.css'
+
+function LoadingPage() {
+	return (
+		<div className="flex w-full flex-col gap-3">
+			<Skeleton className="mb-4" height={60} width={500} />
+			<Skeleton height={50} width={350} />
+			<Skeleton height={50} width={350} />
+			<Skeleton height={50} width={350} />
+		</div>
+	);
+}
+
+export default LoadingPage;
--- src/app/(dashboard)/dashboard/requests/page.tsx
+++ src/app/(dashboard)/dashboard/requests/page.tsx
@@ -28,7 +28,7 @@ async function FriendRequestsPage() {
 	);
 	return (
 		<main className="pt-8">
-			<h1 className="font-bold text-5xl mb-8">Add a friend</h1>
+			<h1 className="font-bold text-5xl mb-8">Friend Requests</h1>
             <div className="flex flex-col gap-4">
                 <FriendRequests incomingFriendRequests={incomingFriendRequests} sessionId={session.user.id}/>
             </div>
